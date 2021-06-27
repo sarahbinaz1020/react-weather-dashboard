4b70412015a95c3bc3f89b067d2d46fea3d58cb9
sarahbinaz1020 <sarahbinaz1020@gmail.com>
2021-06-26 20:01:15 -0700

    footer.js set-up

--- src/components/CurrentDay/CurrentDay.js
+++ src/components/CurrentDay/CurrentDay.js
@@ -3,7 +3,7 @@ import React from 'react'
 function CurrentDay() {
     return (
         <div>
-            
+            current forecast
         </div>
     )
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
     return (
         <div>
-            
+            Est. 2001
         </div>
     )
 }
