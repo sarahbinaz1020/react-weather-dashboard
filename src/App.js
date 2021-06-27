e82a5b4e89de53ebe4aafe77959175dfc8fc0b0e
sarahbinaz1020 <sarahbinaz1020@gmail.com>
2021-06-26 19:22:12 -0700

    App.js base set-up

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import React from "react";
 import "./App.css";
 // import API from "./utils/API";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import { HashRouter as Router, Route } from "react-router-dom";
 import NavBar from "./components/Navbar/NavBar";
 import Wrapper from "./components/Wrapper/Wrapper";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Daily from "./pages/Daily/Daily";
+import Footer from "./components/Footer/Footer"
 
 function App() {
   return (
     <>
       <Router>
         <NavBar />
-        <Container fluid>
-          <Row>
-            <Col>1 of 1</Col>
-          </Row>
+        <Container className="App-header" fluid>
           <Wrapper>
             <Route exact path="/" component={Dashboard} />
             <Route path="/Daily" component={Daily} />
           </Wrapper>
         </Container>
       </Router>
+      <Footer/>
     </>
   );
 }
--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import "./dashboard.css"
 
 function Dashboard() {
     return (
         <div>
-            
+            Dashboard
         </div>
     )
 }
