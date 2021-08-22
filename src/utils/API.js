9fc76b5915baab5b91423f2b53ff59ef50d43027
sarahbinaz1020 <sarahbinaz1020@gmail.com>
2021-08-22 11:38:30 -0700

    working on API

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,11 +1,11 @@
-import axios from "axios";
+// import axios from "axios";
 
-const API = {
-    // Gets current weather
-    getWeather: function () {
-      return axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`);
-    },
-  };
+// const API = {
+//     // Gets current weather
+//     getWeather: function () {
+//       return axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`);
+//     },
+//   };
   
-  export default API;
+//   export default API;
   
