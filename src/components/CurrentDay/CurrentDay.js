ffb6219468e76fa30ed0d77c511d4dac9fd0835c
sarahbinaz1020 <sarahbinaz1020@gmail.com>
2021-10-10 18:24:42 -0700

    adding state for lat & long

--- src/components/CurrentDay/CurrentDay.js
+++ src/components/CurrentDay/CurrentDay.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import "./currentday.css";
 
-
 function CurrentDay() {
-    return (
-        <div>
-            HELLO this is current day
-        </div>
-    )
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
+
+    console.log("Latitude is:", lat);
+    console.log("Longitude is:", long)}, [lat, long]);
+
+
+
+  return <div>HELLO this is current day</div>;
 }
 
-export default CurrentDay
+export default CurrentDay;
